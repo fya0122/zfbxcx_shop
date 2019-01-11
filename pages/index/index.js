b0ec9bc848c65c98191634797839b1ba1318fad2
fya0122 <18730930122@163.com>
2019-01-11 20:40:00 +0800

    '商城特色'

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,5 @@
 Page({
   data: {
-    imgUrls: [{ url: '/resources/img/1.png', id: 0 }, { url: '/resources/img/4.png', id: 1 }]
+    imgUrls: [{ url: '/resources/img/4.png', id: 0 }, { url: '/resources/img/1.png', id: 1 }]
   }
 });
