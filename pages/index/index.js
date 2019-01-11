09f968acd29b2764f4ea830e8472816e01752e86
fya0122 <18730930122@163.com>
2019-01-12 00:23:29 +0800

    'new_ok'

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,11 +2,13 @@ const app = getApp()
 Page( {
   data: {
     imgUrls: [],
-    recList: []
+    recList: [],
+    newList: []
   },
   onLoad () {
-    this._getSwiperData() // 轮播图
+    this._getSwiperData() // 得到轮播图的数据
     this._getRecData() // 得到推荐的数据
+    this._getNewData() // 得到新品的数据
   },
   _getSwiperData () {
     my.httpRequest({
@@ -15,7 +17,9 @@ Page( {
       success:((res) => {
         if (res.data.msg === 'OK' && res.data.status === 200 && res.data.data) {
           this.setData({
-            imgUrls: res.data.data.map(item => {
+            imgUrls: res.data.data.filter((item) => {
+              return item.imageUrl.length > 0
+            }).map(item => {
               return {
                 id: item.id,
                 imageUrl: item.imageUrl
@@ -52,8 +56,52 @@ Page( {
               }
             })
           })
+        } else {
+          this.setData({
+            recList: []
+          })
         }
+      }),
+      fail: ((err) => {
+        console.log(err)
+        this.setData({
+          recList: []
+        })
       })
     })
+  },
+  _getNewData () {
+    my.httpRequest({
+      url: app.baseServerUrl + '/index/items/new',
+      method: 'POST',
+      success: (res) => {
+        if (res.data.msg === 'OK' && res.data.status === 200 && res.data.data) {
+          this.setData({
+            newList: res.data.data.filter((item) => {
+              return item.isRecommend === false && item.isNew === true
+            }).map(item => {
+              return {
+                catId: item.catId,
+                cover: item.cover,
+                likeCounts: item.likeCounts,
+                priceNormalYuan: item.priceNormalYuan,
+                priceDiscountYuan: item.priceDiscountYuan,
+                tagList: item.tagList.slice(0, 4)
+              }
+            })
+          })
+        } else {
+          this.setData({
+            newList: []
+          })
+        }
+      },
+      fail: ((err) => {
+        console.log(err)
+        this.setData({
+          newList: []
+        })
+      })
+    });
   }
 } );
