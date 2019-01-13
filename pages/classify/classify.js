b3492f8bec039a9af93f8491a1ad2a683af0e270
fya0122 <18730930122@163.com>
2019-01-13 14:02:25 +0800

    '分类接口_ok'

--- pages/classify/classify.js
+++ pages/classify/classify.js
@@ -1,4 +1,34 @@
+const app = getApp()
 Page({
-  data: {},
-  onLoad() {},
+  data: {
+    cats: []
+  },
+  onLoad() {
+    this._getCatsData()
+  },
+  _getCatsData () {
+    my.httpRequest({
+      url: app.baseServerUrl + '/cats',
+      method: 'POST',
+      success: ((res) => {
+        if (res.data.status === 200 && res.data.msg === 'OK' && res.data.data) {
+          this.setData({
+            cats: res.data.data.map((item) => {
+              return {
+                code: item.code.toLowerCase(),
+                id: item.id,
+                name: item.name
+              }
+            })
+          })
+        }
+      }),
+      fail: ((err) => {
+        console.log(err)
+        this.setData({
+          cats: []
+        })
+      })
+    });
+  }
 });
