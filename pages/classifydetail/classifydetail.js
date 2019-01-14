60df24f47612bb47e72ebecc21ebad47a433815e
fya0122 <18730930122@163.com>
2019-01-14 22:54:45 +0800

    'detail_keepOn'

--- pages/classifydetail/classifydetail.js
+++ pages/classifydetail/classifydetail.js
@@ -1,6 +1,8 @@
 const app = getApp()
 Page({
-  data: {},
+  data: {
+    currentObj: {}
+  },
   onLoad(e) {
     const id = e.id
     if (id) {
@@ -8,6 +10,7 @@ Page({
     }
   },
   _getSpecificDataById (id) {
+    my.showNavigationBarLoading();
     my.httpRequest({
       url: app.baseServerUrl + '/items/searchById',
       data: {
@@ -16,11 +19,33 @@ Page({
       method: 'POST',
       success: ((res) => {
         if (res.data.status === 200 && res.data.msg === 'OK' && res.data.data) {
-          console.log(res.data.data)
+          const item = res.data.data
+          this.setData({
+            currentObj: {
+                catId: item.catId,
+                cover: item.cover,
+                coverList: item.coverList,
+                discounts: item.discounts,
+                headerImages: typeof(item.headerImages) === 'string' ? JSON.parse(item.headerImages) : item.headerImages,
+                id: item.id,
+                likeCounts: item.likeCounts,
+                name: item.name,
+                priceDiscountYuan: item.priceDiscountYuan,
+                priceNormalYuan: item.priceNormalYuan,
+                tagList: item.tagList,
+                sellCounts: item.sellCounts
+            }
+          })
         }
       }),
       fail: ((err) => {
         console.log(err)
+        this.setData({
+          currentObj: {}
+        })
+      }),
+      complete: (() => {
+        my.hideNavigationBarLoading();
       })
     });
   }
