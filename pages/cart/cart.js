b69e3d686ce20f2b8e17a26b7d7652c0e778d267
fya0122 <18730930122@163.com>
2019-01-17 22:55:54 +0800

    'all_else_ok'

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,9 +1,10 @@
 const app = getApp()
 Page({
   data: {
-    emptyHidden: true
+    cartList: [],
+    isHasValue: false // 默认没有值的
   },
-  onLoad() {
+  onShow() {
     this._getShoppingCartData()
   },
   helpYourSelf () {
@@ -12,10 +13,71 @@ Page({
     });
   },
   _getShoppingCartData () {
-    my.httpRequest({
-      url: app.baseServerUrl + '',
-      success: (res) => {  
+    const cart = my.getStorageSync({ key: 'cart_item_id_array' }).data
+    if (cart) {
+      my.showNavigationBarLoading()
+      if (Array.isArray(cart) === true) {
+        let itemIds = ''
+        for (const item of cart) {
+          itemIds += item.id + ','
+        }
+        my.httpRequest({
+          url: app.baseServerUrl + '/item/queryItems',
+          data: {
+            itemIds: itemIds
+          },
+          method: 'POST',
+          success: ((res) => {
+            if (res.data.status === 200 && res.data.msg === 'OK' && res.data.data) {
+              for (const item_http of res.data.data) {
+                for (const item_local of cart) {
+                  if (item_http.id === item_local.id) {
+                    item_http.counts = item_local.counts
+                  }
+                }
+              }
+              this.setData({
+                isHasValue: true,
+                cartList: res.data.data.map(item => {
+                  return {
+                    catId: item.catId,
+                    cover: item.cover,
+                    discounts: item.discounts,
+                    id: item.id,
+                    likeCounts: item.likeCounts,
+                    name: item.name,
+                    priceDiscountYuan: item.priceDiscountYuan,
+                    priceNormalYuan: item.priceNormalYuan,
+                    sellCounts: item.sellCounts,
+                    tagList: item.tagList,
+                    counts: item.counts
+                  }
+                })
+              })
+            } else {
+              this.setData({
+                isHasValue: false,
+                cartList: []
+              })
+            }
+          }),
+          fail: ((err) => {
+            console.log(err)
+            this.setData({
+              isHasValue: false,
+              cartList: []
+            })
+          }),
+          complete: (() => {
+            my.hideNavigationBarLoading();
+          })
+        });
       }
-    });
+    } else {
+      this.setData({
+        isHasValue: false,
+        cartList: []
+      })
+    }
   }
 });
