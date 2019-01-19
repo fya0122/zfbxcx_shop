84fe1f7d47ed9a8a2fd8d6f6e645267966b3067c
fya0122 <18730930122@163.com>
2019-01-20 00:19:07 +0800

    'confirmorder_half_ok'

--- pages/confirmorder/confirmorder.js
+++ pages/confirmorder/confirmorder.js
@@ -1,7 +1,21 @@
 Page({
-  data: {},
-  onLoad() {
-    const data = my.getStorageSync({ key: 'pcart_item_id_array' }).data;
-    console.log(data)
+  data: {
+    confirmOrder: [],
+    totalPrice: 0
   },
+  onLoad() {
+    this._getConfirmOrderData()
+  },
+  _getConfirmOrderData () {
+    let confirmOrder = my.getStorageSync({ key: 'pcart_item_id_array' }).data;
+    let totalPrice = 0
+    for (const item of confirmOrder) {
+      totalPrice += item.counts * parseInt(item.priceDiscountYuan)
+    }
+    this.setData({
+      confirmOrder: confirmOrder,
+      totalPrice: totalPrice
+    })
+    console.log(this.data.confirmOrder)
+  }
 });
