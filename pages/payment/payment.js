13183d5e331c3b8fb19205ee6343e8f044735f50
fya0122 <18730930122@163.com>
2019-01-26 00:45:57 +0800

    '就差支付了'

--- pages/payment/payment.js
+++ pages/payment/payment.js
@@ -1,3 +1,4 @@
+const app = getApp()
 Page({
   data: {
     orderid: '',
@@ -11,6 +12,30 @@ Page({
         orderid: orderid,
         orderprice: orderprice
       })
+    }
+  },
+  // 支付宝支付
+  doAlipay () {
+    if (this.data.orderid) {
+      my.httpRequest({
+        url: app.baseServerUrl + `/team/alipay?orderId=${this.data.orderid}&qq=1144642211`,
+        header: {
+          'content-type': 'application/json'
+        },
+        dataType: 'json',
+        method: 'POST',
+        success: ((res) => {
+          console.log(res)
+        })
+      });
+    } else {
+      my.alert({
+        title: '系统错误!错误码：1029' 
+      });
+      my.switchTab({
+        url: '../index/index'
+      });
+      my.clearStorageSync();
     }
   }
 });
